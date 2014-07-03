311212e72324c1a84329d489a724106efb66b2fb
droquo <drew.s.gordon@gmail.com>
2014-07-03 15:26:00 -0400

    get the images

--- scripts/cdm_getimages.py
+++ scripts/cdm_getimages.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python
 
+import urllib
+import os
+import csv
+
 '''Those pesky contentdm images have been lost and you just want to grab them off the server...well their shruken derivatives that is, just so you can develop. This will go through a processed set of records exported from cdm and get their images from off the server, that's all'''
 
 
+path_to_csv = "../capsule/mcny_ephemera/metadata/" #hardwired for now
+csv_file = "nymc_ephemera_cdm-ref-processed.csv"
+image_path = "../capsule/mcny_ephemera/images/"
+
+
+_FILE = path_to_csv + csv_file
+
+
+
+with open(_FILE, 'rb') as f:
+	r = csv.reader(f)
+
+	r.next()
+
+	for row in r:
+
+		image_dest = image_path + row[3]
+
+		urllib.urlretrieve(row[1], image_dest)
+			
